61900289e3dbef1636fbddfac4dbb3fd1e8f7d63
孔余 <2385569970@qq.com>
2025-01-13 11:47:55 +0800

    数据库模块-RedisTemplate的使用

---
+++ database/redis-template/src/main/java/local/ateng/java/redis/DBRedisTemplateConfigApplication.java
@@ -0,0 +1,13 @@
+package local.ateng.java.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBRedisTemplateConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBRedisTemplateConfigApplication.class, args);
+    }
+
+}
