f01497c17abd08fe081f62a40f920ae4b5d36c7b
孔余 <2385569970@qq.com>
2025-01-13 17:24:02 +0800

    数据库模块-Spring JDBC Template的使用

---
+++ database/jdbc-template/src/main/java/local/ateng/java/jdbc/DBJDBCTemplateConfigApplication.java
@@ -0,0 +1,13 @@
+package local.ateng.java.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DBJDBCTemplateConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DBJDBCTemplateConfigApplication.class, args);
+    }
+
+}
