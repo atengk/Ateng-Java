3a3cb477559f4b0b6347a0f08ccd8dc74fd9ee05
孔余 <2385569970@qq.com>
2025-11-05 16:58:06 +0800

    fix(springboot3\serialize-fastjson): 完善序列化和反序列配置

--- springboot3/serialize-fastjson/src/main/java/local/ateng/java/serialize/controller/RedisController.java
+++ springboot3/serialize-fastjson/src/main/java/local/ateng/java/serialize/controller/RedisController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -41,12 +42,41 @@ public class RedisController {
         return "ok";
     }
 
+    @GetMapping("/serializeList")
+    public String serializeList() {
+        MyUser myUser = MyUser.builder()
+                .id(1L)
+                .name("ateng")
+                .age(null)
+                .phoneNumber("1762306666")
+                .email("kongyu2385569970@gmail.com")
+                .score(new BigDecimal("88.911"))
+                .ratio(0.7147)
+                .birthday(LocalDate.parse("2000-01-01"))
+                .province(null)
+                .city("重庆市")
+                .createTime(LocalDateTime.now())
+                .createTime2(new Date())
+                .list(List.of("1","2"))
+                .build();
+        redisTemplate.opsForValue().set("myUserList", Collections.singletonList(myUser));
+        return "ok";
+    }
+
     // 反序列化
     @GetMapping("/deserialize")
     public String deserialize() {
         MyUser myUser = (MyUser) redisTemplate.opsForValue().get("myUser");
         System.out.println(myUser);
         System.out.println(myUser.getCreateTime());
+        return "ok";
+    }
+
+    // 反序列化
+    @GetMapping("/deserializeList")
+    public String deserializeList() {
+        List<MyUser> myUserList = (List<MyUser>) redisTemplate.opsForValue().get("myUserList");
+        System.out.println(myUserList);
         return "ok";
     }
 
