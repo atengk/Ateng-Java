920797be67cca05c59aee7d5dc8f27f3fe829519
孔余 <2385569970@qq.com>
2026-03-28 11:03:30 +0800

    feat(springboot4\boot4-config): SpringBoot4配置模块更新版本

--- springboot4/boot4-config/src/main/java/io/github/atengk/config/SpringBoot4ConfigApplication.java
+++ springboot4/boot4-config/src/main/java/io/github/atengk/config/SpringBoot4ConfigApplication.java
@@ -1,4 +1,4 @@
-package local.ateng.java.config;
+package io.github.atengk.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
