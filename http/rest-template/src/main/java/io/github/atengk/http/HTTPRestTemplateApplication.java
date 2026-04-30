6d6751d8a2f6cf641c2a3e9f1170bdce800daea0
孔余 <2385569970@qq.com>
2026-04-30 17:49:22 +0800

    feat(http\rest-template): RestTemplate 开发使用文档

--- http/rest-template/src/main/java/io/github/atengk/http/HTTPRestTemplateApplication.java
+++ http/rest-template/src/main/java/io/github/atengk/http/HTTPRestTemplateApplication.java
@@ -1,4 +1,4 @@
-package local.ateng.java.rest;
+package io.github.atengk.http;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
